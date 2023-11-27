95675a396f978f362067f3a5b88f531ebe94c7d7
zoranbogoevski <zoran@orangemelon.com>
2023-11-27 20:06:43 +0100

    Simplified Condition Logic: Replaced the when() method with a simple if-else condition. This makes the logic more straightforward and easier to read. Return Statement: Ensured that the macro method explicitly returns $this to maintain the fluid interface of the query builder. Removed Redundant Closure: The second closure passed to when() in the original code was not necessary because the alternative case can be handled directly in the else block of the if-else statement.

--- src/FilterableServiceProvider.php
+++ src/FilterableServiceProvider.php
@@ -15,24 +15,18 @@ class FilterableServiceProvider extends ServiceProvider
     public function boot()
     {
         Builder::macro('whereLike', function ($attributes, string $searchTerm) {
-            $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+            return $this->where(function (Builder $query) use ($attributes, $searchTerm) {
                 foreach (Arr::wrap($attributes) as $attribute) {
-                    $query->when(
-                        str_contains($attribute, '.'),
-                        function (Builder $query) use ($attribute, $searchTerm) {
-                            [$relationName, $relationAttribute] = explode('.', $attribute);
-                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
-                                $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
-                            });
-                        },
-                        function (Builder $query) use ($attribute, $searchTerm) {
-                            $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
-                        }
-                    );
+                    if (str_contains($attribute, '.')) {
+                        [$relationName, $relationAttribute] = explode('.', $attribute);
+                        $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
+                            $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
+                        });
+                    } else {
+                        $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                    }
                 }
             });
-
-            return $this;
         });
     }
 }
